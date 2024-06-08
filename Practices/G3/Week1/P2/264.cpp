db323163a2202218a01ab872c19fe3f895111fc6
Nikita Ussyukin <n.ussyukin@kbtu.kz>
2024-06-08 12:48:51 +0500

    G3 P2

---
+++ Practices/G3/Week1/P2/264.cpp
@@ -0,0 +1,39 @@
+/*
+Ice cream portions - 3 and/or 5
+
+Possible numbers from 0 to 9: 0 1 2 3 4 5 6 7 8 9
+Suitable numbers: 3 5 6 8 9
+
+10 = 5 * 2
+11 = 6 + 5
+12 = 3 * 4
+13 = 5 * 2 + 3
+14 = 9 + 5
+15 = 5 * 3
+16 = 6 + 5 * 2
+17 = 3 * 4 + 5
+18 = 3 * 6
+19 = 5 * 2 + 9
+20 = 5 * 4
+21 = 3 * 7
+22 = 3 * 4 + 5 * 2
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int k;
+    cin >> k;
+
+    if(k < 3 || k == 4 || k == 7) {
+        cout << "NO\n";
+    } 
+    else {
+        cout << "YES\n";
+    }
+
+    return 0;
+}
---
+++ Practices/G3/Week1/P2/lab1_b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k; 
+
+    cin >> n >> k;
+    // 723 - how to get 7?
+    cout << "k / 100: " << k / 100 << endl;
+
+    // 723 - how to get 3?
+    cout << "k % 10: " << k % 10 << endl;
+
+    return 0;
+}
