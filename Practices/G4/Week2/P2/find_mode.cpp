d076503955dd63217d3950350b24dc4baba20e31
Nikita Ussyukin <n.ussyukin@kbtu.kz>
2024-06-15 19:56:10 +0500

    G4 P2

---
+++ Practices/G4/Week2/P2/find_mode.cpp
@@ -0,0 +1,38 @@
+// Using array elements as counters
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // assume we can get digits between 0 and 9 as an input
+    int arraySize = 10;
+    int a[arraySize] = {}; // "= {}" means fill the array with zeroes; it only works when you know the size of the array before compilation
+    // if the array has the size 10, then it has
+    // indexes in the range [0; 9]
+
+    int n; // in this case, n means the amount of digits we will get
+    cin >> n;
+
+    for(int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        ++a[x];
+    }
+
+    // counter array contents
+    // for(int i = 0; i < arraySize; ++i) {
+    //     cout << "i: " << i << ", count: " << a[i] << endl;
+    // }
+
+    int mode = 0;
+
+    for(int i = 0; i < arraySize; ++i) {
+        if(a[mode] < a[i]) mode = i; 
+    }
+    
+    cout << mode << endl;
+
+    return 0;
+}
