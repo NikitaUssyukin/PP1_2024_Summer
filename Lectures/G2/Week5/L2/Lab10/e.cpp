1ac7df5d71994b6b7a13cd0775b6f9b42d648fa9
NikitaUssyukin <nikita.ussyukin@gmail.com>
2024-07-06 12:22:59 +0500

    G2 L2

--- Lectures/G2/Week5/L2/Lab10/e.cpp
+++ Lectures/G2/Week5/L2/Lab10/e.cpp
@@ -11,6 +11,8 @@ int main() {
     int n;
     cin >> n;
 
+    
+
     for(int i = 0; i < n; ++i) {
         int m;
         cin >> m;
--- Lectures/G2/Week5/L2/Lab9/o.cpp
+++ Lectures/G2/Week5/L2/Lab9/o.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    // freopen("output.txt", "w", stdout);
+    freopen("output.txt", "w", stdout);
 
     map<string, string> users;
 
